6ee24ecca710972cec81771a7a7f4198c403bf26
Kieran-byte <bettesworthkieran1@gmail.com>
2024-03-22 19:22:38 +1300

    Removed obsolete imports

--- src/Pages/TrendingPage/TrendingPage.js
+++ src/Pages/TrendingPage/TrendingPage.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import './TrendingPage.css';
 import TrendingItem from '../../Components/TrendingItem/TrendingItem';
-import NZMap from '../../images/nz.jpg'
 import InteractiveMap from '../../Components/map/InteractiveMap';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer'
@@ -23,8 +22,6 @@ const TrendingPage = () => {
           <TrendingItem title="Item 4" description="Description of item 4." />
           <TrendingItem title="Item 5" description="Description of item 5." />
           <TrendingItem title="Item 6" description="Description of item 6." />
-          <TrendingItem title="Item 7" description="Description of item 7." />
-          <TrendingItem title="Item 8" description="Description of item 8." />
           {/* etc */}
         </div>
         <div className="section browse">
@@ -33,13 +30,11 @@ const TrendingPage = () => {
         </div>
         <div className="section add">
           <button>Post An Issue</button>
-          {/* Add content goes here */}
         </div>
       </div>
       <div className="large-column">
         <div className="section image">
             <InteractiveMap/>
-          {/* <img src={NZMap} className='image' alt="Large Column Image" /> */}
         </div>
       </div>
     </div>
