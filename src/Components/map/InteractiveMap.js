ae76b35746441a5b79f70a0ecc3d48907d2311aa
Kieran-byte <bettesworthkieran1@gmail.com>
2024-03-25 11:22:37 +1300

    Got map rendering correctly

--- src/Components/map/InteractiveMap.js
+++ src/Components/map/InteractiveMap.js
@@ -1,23 +1,22 @@
 // InteractiveMap.js
-
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './InteractiveMap.css'
 
 const InteractiveMap = () => {
+
   return (
-    <div style={{ height: '400px', width: '100%' }}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <MapContainer className='leaflet-container' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   );
 };
 
